67836acad71a0686b1129c987700a8ed4f927bbd
Kushagra Gupta <kushagragupta853@gmail.com>
2025-03-03 12:31:08 +0530

    [Kushagra] Add. Address Repository program in UC2

---
+++ src/main/java/com/example/addressBookApp/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.example.addressBookApp.repository;
+
+import com.example.addressBookApp.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
